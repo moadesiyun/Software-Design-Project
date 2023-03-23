21aebadde19d3a99957141488b1262ded7959c54
Monboluwarin Adesiyun <88043947+moadesiyun@users.noreply.github.com>
2023-03-23 15:49:55 -0500

    commit 3

--- website/models.py
+++ website/models.py
@@ -2,13 +2,25 @@ from . import db
 from flask_login import UserMixin
 from sqlalchemy.sql import func
 
-
+"""
 class Note(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     data = db.Column(db.String(10000))
     date = db.Column(db.DateTime(timezone=True), default=func.now())
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+"""
 
+class Profile(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    fname = db.Column(db.String(50))
+    lname = db.Column(db.String(50))
+    userAddress1 = db.Column(db.String(100))
+    userAddress2 = db.Column(db.String(100))
+    city = db.Column(db.String(15))
+    state  = db.Column(db.String(2))
+    zipcode = db.Column(db.Integer())
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
