5748850c356a4bca7dddaa6d5b737cd5415154e1
Monboluwarin Adesiyun <88043947+moadesiyun@users.noreply.github.com>
2023-03-23 15:54:00 -0500

    6

--- website/views.py
+++ website/views.py
@@ -18,22 +18,19 @@ def home():
 @login_required
 def profile():
     if request.method == 'POST':
-        if request.method == 'POST':
-        fname = request.form.get('fname')
-        lname = request.form.get('lname')
+        fName = request.form.get('fname')
+        lName = request.form.get('lname')
         userAdd1 = request.form.get('userAddress1')
         userAdd2 = request.form.get('userAddress2')
         uCity = request.form.get('city')
         st = request.form.get('state')
         zipcd = request.form.get('password2')
-        if len(lname) < 4:
-            flash('Email must be greater than 3 characters.', category='error')
-        elif len(uCity) < 5:
+        if len(uCity) < 5:
             flash('Passwords don\'t match.', category='error')
         elif len(zipcd) < 5:
             flash('Zipcode must be at least 7 characters.', category='error')
         else:
-            updated_profile = Profile(fname=fname, lname =lname, userAddress1 =userAdd1, userAddress2 =userAdd2, city=uCity, state=st, zipcode=zipcd, user_id=current_user.id)  #providing the schema for the note 
+            updated_profile = Profile(fname=fName, lname =lName, userAddress1 =userAdd1, userAddress2 =userAdd2, city=uCity, state=st, zipcode=zipcd, user_id=current_user.id)  #providing the schema for the note 
             db.session.add(updated_profile) #adding the note to the database 
             db.session.commit()
             flash('Profile information updated!', category='success')
