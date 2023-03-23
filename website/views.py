cbe8ec1b6439d3aa520154910fe8b1884304dd75
Monboluwarin Adesiyun <88043947+moadesiyun@users.noreply.github.com>
2023-03-23 18:21:28 -0500

    7

--- website/views.py
+++ website/views.py
@@ -36,12 +36,22 @@ def profile():
             
     return render_template("profile.html", user=current_user)
 
-@views.route('/quote-form', methods=['GET', 'POST'])
+@views.route('/quote-form')
 @login_required
 def form():
     return render_template("quote-form.html", user=current_user)
 
-@views.route('/quote-history', methods=['GET', 'POST'])
+@views.route('/quote-history')
 @login_required
 def history():
     return render_template("quote-history.html", user=current_user)
+
+@views.route('/get-quote-history', methods=['GET', 'POST'])
+@login_required
+def get_fuel_quote_history():
+    fuel_quote_history = [
+        {'date': '2022-03-15', 'gallons': 100,'address': '4401 Cougar Village Drive','delivery_date': '2022-04-04', 'msrp': 258.67, 'total_price': 250.00},
+        {'date': '2022-02-22', 'gallons': 75,'address': '3875 Holman Street', 'delivery_date': '2022-03-6', 'msrp': 190.00, 'total_price': 187.50},
+        {'date': '2022-01-28', 'gallons': 50,'address': '4300 University Drive','delivery_date': '2022-03-07', 'msrp': 126.50, 'total_price': 125.00}
+    ]
+    return render_template("quote-history.html", fuel_quote_history=fuel_quote_history, user=current_user)
