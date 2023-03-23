639d3ab178be1caf55d32d988444647e3fdc4ef9
Monboluwarin Adesiyun <88043947+moadesiyun@users.noreply.github.com>
2023-03-22 23:29:33 -0500

    Update auth.py

--- website/auth.py
+++ website/auth.py
@@ -69,29 +69,20 @@ def sign_up():
 @login_required
 def profile():
     if request.method == 'POST':
-        email = request.form.get('email')
-        first_name = request.form.get('firstName')
-        password1 = request.form.get('password1')
-        password2 = request.form.get('password2')
-
-        user = User.query.filter_by(email=email).first()
-        if user:
-            flash('Email already exists.', category='error')
-        elif len(email) < 4:
+        lname = request.form.get('lname')
+        userAddress1 = request.form.get('userAddress1')
+        city = request.form.get('city')
+        zipcode = request.form.get('password2')
+        if len(lname) < 4:
             flash('Email must be greater than 3 characters.', category='error')
-        elif len(first_name) < 2:
-            flash('First name must be greater than 1 character.', category='error')
-        elif password1 != password2:
+        elif len(userAddress1) < 2:
+            flash('Enter valid street address', category='error')
+        elif len(city) < 5:
             flash('Passwords don\'t match.', category='error')
-        elif len(password1) < 7:
-            flash('Password must be at least 7 characters.', category='error')
+        elif len(zipcode) < 5:
+            flash('Zipcode must be at least 7 characters.', category='error')
         else:
-            new_user = User(email=email, first_name=first_name, password=generate_password_hash(
-                password1, method='sha256'))
-            db.session.add(new_user)
-            db.session.commit()
-            login_user(new_user, remember=True)
-            flash('Account created!', category='success')
+            flash('Profile information updated!', category='success')
             return redirect(url_for('views.home'))
     return render_template("profile.html", user=current_user)
 
@@ -103,5 +94,4 @@ def form():
 @auth.route('/quote-history', methods=['GET', 'POST'])
 @login_required
 def history():
-    if request.method == 'GET':
-        return render_template("quote-history.html", user=current_user)
+    return render_template("quote-history.html", user=current_user)
