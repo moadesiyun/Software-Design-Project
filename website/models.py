1220e3331bd49685b7730a2302b30c78e390e978
Monboluwarin Adesiyun <88043947+moadesiyun@users.noreply.github.com>
2023-03-24 13:24:25 -0500

    Update models.py

--- website/models.py
+++ website/models.py
@@ -2,13 +2,7 @@ from . import db
 from flask_login import UserMixin
 from sqlalchemy.sql import func
 
-"""
-class Note(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    data = db.Column(db.String(10000))
-    date = db.Column(db.DateTime(timezone=True), default=func.now())
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-"""
+"
 
 class Profile(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -24,7 +18,6 @@ class Profile(db.Model):
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(150), unique=True)
+    uname = db.Column(db.String(20), unique=True)
     password = db.Column(db.String(150))
     first_name = db.Column(db.String(150))
-    notes = db.relationship('Note')
