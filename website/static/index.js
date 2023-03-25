0cc04be56199936e0d328d7b4f295fd57231b354
= <ghines1254@gmail.com>
2023-03-24 21:50:20 -0500

    we got it

--- website/static/index.js
+++ website/static/index.js
@@ -6,3 +6,21 @@ function deleteNote(noteId) {
     window.location.href = "/";
   });
 }
+
+function ajaxpost () {
+  var data = new FormData(document.getElementById("quote-form"));
+ 
+  fetch("1c-server.html", { method:"POST", body:data })
+  .then(res => res.text())
+ 
+  .then(response => {
+    console.log(response);
+    //Hard coded until db finished
+    if (response == "OK") { alert("Total Amount Due is: $2542.50"); }
+    else { alert("FAILURE!"); }
+  })
+ 
+  .catch(err => console.error(err));
+ 
+  return false;
+}
