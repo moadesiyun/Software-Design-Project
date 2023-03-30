f35953f27b7d7899a847ee6879eaa057b137045d
jurjones29 <68124259+jurjones29@users.noreply.github.com>
2023-03-30 10:35:41 -0500

    db stuff

--- website/__init__.py
+++ website/__init__.py
@@ -1,10 +1,16 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 from os import path
 
+db = SQLAlchemy()
+DATABASE = "database.db"
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'software-design proj'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DATABASE}'
+    db.init_app(app)
+    
     from .views import views
     from .auth import auth
     from .requestForm import requestForm
@@ -13,6 +19,10 @@ def create_app():
     app.register_blueprint(auth, url_prefix='/')
     app.register_blueprint(requestForm, url_prefix='/')
 
+    from .dbmodels import Client
+
+    with app.app_context():
+        db.create_all()
     
     return app
 
