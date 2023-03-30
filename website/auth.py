56d4eff5f75f73fe0546cf782644d379a1361aa7
jurjones29 <68124259+jurjones29@users.noreply.github.com>
2023-03-30 10:36:49 -0500

    Update auth.py

--- website/auth.py
+++ website/auth.py
@@ -1,5 +1,7 @@
 from flask import Blueprint, render_template, request, flash, redirect, url_for
 from .client import Client, profiles
+from .dbmodels import Client
+from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import datetime
 
 auth = Blueprint('auth', __name__)
