0799efa2e1c7cd769bb143d7acd53efc32c5a097
jurjones29 <68124259+jurjones29@users.noreply.github.com>
2023-03-30 10:33:44 -0500

    models file

---
+++ website/dbmodels.py
@@ -0,0 +1,10 @@
+from website import db
+from flask_login import UserMixin
+from datetime import datetime
+
+
+class Client(db.Model, UserMixin):
+    Id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100), unique = True)
+    password = db.Column(db.String(100))
+    loginTime = db.Column(db.DateTime())
