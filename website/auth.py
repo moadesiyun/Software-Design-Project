ed4b19dbc6f38495ca5cb0b9959c7923c53ed3e8
jurjones29 <68124259+jurjones29@users.noreply.github.com>
2023-04-01 16:11:24 -0500

    login and sign up w db

--- website/auth.py
+++ website/auth.py
@@ -1,8 +1,9 @@
 from flask import Blueprint, render_template, request, flash, redirect, url_for
 from .client import Client, profiles
-from .dbmodels import ClientDB
+from .dbmodels import userCredentials
+from . import db
 from werkzeug.security import generate_password_hash, check_password_hash
-from datetime import datetime
+from flask_login import login_user, login_required, logout_user, current_user
 
 auth = Blueprint('auth', __name__)
 
@@ -13,32 +14,36 @@ def login():
         username = request.form.get('username')
         password = request.form.get('password')
 
-        for client in profiles:
-            if client.username == username and client.password == password:
-                curr_client = client
-                profiles.remove(client)
-                profiles.append(curr_client)
-                
-                if client.logintime == None:
-                    #first time user detected
-                    logintime = datetime.now()
-                    client.logintime = logintime
-                    flash('Logged in successfully! Please create your profile.', category='success')
-                    return render_template("profile.html", userauthentic = True)
+        client = userCredentials.query.filter_by(username=username).first()
 
-                    #take other clients to home page
-                logintime = datetime.now()
-                client.logintime = logintime
-                flash('Logged in successfully!', category='success')
-                return render_template("home.html", userauthentic = True)
-                
-               
-        flash('Credentials are incorrect. Please try again.', category='error')
+        if client:
+            if check_password_hash(client.password, password):
+
+                if client.loginTime == False:
+                    client.loginTime = True
+                    db.session.commit()
+                    flash('Logged in successfully! Please create your profile.', category='success')
+                    login_user(client, remember = True)
+                    #redirect to profile page
+                    return redirect(url_for('auth.profile',userauthentic = True))
+                    
+                else:
+                    flash('Logged in successfully!', category='success')
+                    login_user(client, remember = True)
+                    #redirect to homepage
+                    return redirect(url_for('views.home',userauthentic = True))
+                   
+            else:
+                 flash('Incorrect password.', category='error')
+        else:
+            flash('Username does not exist.', category='error')
+
     return render_template("login.html", userauthentic = False)
-    
 
 @auth.route('/logout')
+@login_required
 def logout():
+    logout_user()
     return redirect(url_for('auth.login',userauthentic = False))
 
 
@@ -47,26 +52,29 @@ def sign_up():
     if request.method == 'POST':
         # Get info from front end. Will eventually persist through DB
         username = request.form.get('username')
-        password1 = request.form.get('password1')
-        password2 = request.form.get('password2')
+        password_1 = request.form.get('password1')
+        password_2 = request.form.get('password2')
         
-        exists = None
-        for client in profiles:
-            if client.username == username:
-                exists = True
+        clientCred = userCredentials.query.filter_by(username=username).first()
 
-        if exists:
-            flash('Username already exists. Please try again', category='error')
-        elif password1 != password2:
-            flash('Passwords don\'t match. Please try again', category='error')
+        if clientCred:
+            flash('Username already exists.', category='error')
+
+        elif len(username) < 5:
+            flash('Username must be 5 or more characters', category='error')
+
+        elif password_1 != password_2:
+            flash('Passwords must match. Try again.', category='error')
+
+        elif len(password_1) < 5:
+            flash('Password must be 5 or more characters', category='error')
+
         else:
-            new_client = Client(username=username, password=password1, logintime = None)
-            profiles.append(new_client) #persist through db
-            #direct clients to log in for the first time
-            flash('Account created successfully! Please Log in.', category='success')
+            new_client = userCredentials(username = username, password= generate_password_hash(password_1, method='sha256'), loginTime = False)
+            db.session.add(new_client)
+            db.session.commit()
+            flash('Account created successfully! Please log in.', category='success')
             return redirect(url_for('auth.login',userauthentic = False))
-
-
             
     return render_template("sign_up.html", userauthentic = False)
 
