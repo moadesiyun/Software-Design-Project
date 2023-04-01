33d954141799e282eb56cb2e11fe518502bb7ddc
jurjones29 <68124259+jurjones29@users.noreply.github.com>
2023-04-01 16:12:07 -0500

    login and sign up w db

--- website/dbmodels.py
+++ website/dbmodels.py
@@ -3,8 +3,9 @@ from flask_login import UserMixin
 from datetime import datetime
 
 
-class ClientDB(db.Model, UserMixin):
-    Id = db.Column(db.Integer, primary_key=True)
+class userCredentials(db.Model, UserMixin):
+
+    id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(100), unique = True)
     password = db.Column(db.String(100))
-    loginTime = db.Column(db.DateTime())
+    loginTime = db.Column(db.Boolean())
