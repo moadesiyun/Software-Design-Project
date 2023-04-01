4aedf1c391c4a43fcc09cb5784c50aa03a1acd37
jurjones29 <68124259+jurjones29@users.noreply.github.com>
2023-04-01 16:13:52 -0500

    login required

--- website/views.py
+++ website/views.py
@@ -1,26 +1,30 @@
 from flask import Blueprint, render_template, request, flash
 from flask_login import login_required, current_user
 from .client import Client, profiles
+from flask_login import login_user, login_required, logout_user, current_user
 import json
 
 views = Blueprint('views', __name__)
 
 
 @views.route('/', methods=['GET', 'POST'])
-
+@login_required
 def home():
     return render_template("home.html",  userauthentic = True)
 
 
 @views.route('/quote-form')
+@login_required
 def form():
     return render_template("quote-form.html",  userauthentic = True)
 
 @views.route('/quote-history')
+@login_required
 def history():
     return render_template("quote-history.html",  userauthentic = True)
 
 @views.route('/get-quote-history', methods=['GET', 'POST'])
+@login_required
 def get_fuel_quote_history():
     fuel_quote_history = [
         {'date': '2022-03-15', 'gallons': 100,'address': '4401 Cougar Village Drive','delivery_date': '2022-04-04', 'msrp': 258.67, 'total_price': 250.00},
