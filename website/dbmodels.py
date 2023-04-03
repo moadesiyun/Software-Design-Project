c9f2e9310dc29b688ad9f5314fade04fdf79efd2
Monboluwarin Adesiyun <88043947+moadesiyun@users.noreply.github.com>
2023-04-02 19:22:42 -0500

    b

--- website/dbmodels.py
+++ website/dbmodels.py
@@ -1,7 +1,15 @@
 from website import db
 from flask_login import UserMixin
 from datetime import datetime
+from sqlalchemy.sql import func
 
+"""
+class ClientProfile(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    data = db.Column(db.String(10000))
+    date = db.Column(db.DateTime(timezone=True), default=func.now())
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+"""
 
 class userCredentials(db.Model, UserMixin):
 
@@ -9,3 +17,4 @@ class userCredentials(db.Model, UserMixin):
     username = db.Column(db.String(100), unique = True)
     password = db.Column(db.String(100))
     loginTime = db.Column(db.Boolean())
+    profile = db.relationship('ClientProfile')
