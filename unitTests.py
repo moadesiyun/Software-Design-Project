1ee694aafe18433537ed3c865e815145a9635146
Gwyneth Hines <ghines1254@gmail.com>
2023-04-10 19:51:55 -0500

    created unit test executable

---
+++ unitTests.py
@@ -0,0 +1,8 @@
+import unittest
+
+#Helpful link to learn/understand unit tests <3
+#https://machinelearningmastery.com/a-gentle-introduction-to-unit-testing-in-python/
+
+#Create class for each test to be completed
+#Basically see if the program, when broken, deals with the invalid input properly (eg. flash error, set to 0, etc.)
+#In our case, we most likely want to make sure the invalid input isn't saved in the profile/calculation/etc.
